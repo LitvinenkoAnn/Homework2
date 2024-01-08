da7b8430d3ceea33478cf699d2281bff0dea1813
LitvinenkoAnn <annalitvinenko299@gmail.com>
2024-01-08 14:13:53 +0200

    Homework2

---
+++ script1.js
@@ -0,0 +1,12 @@
+
+let goal = 5;
+let milesRan = 4;
+
+let result = goal - milesRan
+console.log("Miles left: " + result);
+
+if (result == 0) {
+    console.log("Success!");
+  } else {
+    console.log("Quite a bit remains to be done!");
+  }
---
+++ script2.js
@@ -0,0 +1,14 @@
+
+const userName = "Mike";
+const age = 28;
+const isStudent = true;
+
+console.log(userName, age);
+
+if (isStudent == true) {
+    console.log("Student");
+  } else {
+    console.log("Not a student");
+  }
+
+
